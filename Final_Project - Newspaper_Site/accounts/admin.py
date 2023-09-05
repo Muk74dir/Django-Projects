c090092d3e6b3a250adef3e15cf209f888065c45
Muk74dir <muktadir.sarker99@gmail.com>
2023-09-06 03:01:38 +0600

    Final_Project

--- Final_Project - Newspaper_Site/accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import UserRegistrationModel
-
-@admin.register(UserRegistrationModel)
-class UserRegistrationAdmin(admin.ModelAdmin):
-    list_display = ['user', 'account_type']
-
-    def __self__(self, obj):
-        return obj.user.username
